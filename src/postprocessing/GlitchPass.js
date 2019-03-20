8e85e7bcfd09e395a36b46c241b571fec3c2643d
Molay Chen <molayc@gmail.com>
2019-03-20 10:31:44 +0800

    fix: Fix GlitchPass "Math" issue.

--- src/postprocessing/GlitchPass.js
+++ src/postprocessing/GlitchPass.js
@@ -1,7 +1,7 @@
 import {
   DataTexture,
   FloatType,
-  Math,
+  Math as ThreeMath,
   Mesh,
   OrthographicCamera,
   PlaneBufferGeometry,
@@ -64,22 +64,22 @@ GlitchPass.prototype = Object.assign( Object.create( Pass.prototype ), {
 		if ( this.curF % this.randX == 0 || this.goWild == true ) {
 
 			this.uniforms[ 'amount' ].value = Math.random() / 30;
-			this.uniforms[ 'angle' ].value = Math.randFloat( - Math.PI, Math.PI );
-			this.uniforms[ 'seed_x' ].value = Math.randFloat( - 1, 1 );
-			this.uniforms[ 'seed_y' ].value = Math.randFloat( - 1, 1 );
-			this.uniforms[ 'distortion_x' ].value = Math.randFloat( 0, 1 );
-			this.uniforms[ 'distortion_y' ].value = Math.randFloat( 0, 1 );
+			this.uniforms[ 'angle' ].value = ThreeMath.randFloat( - Math.PI, Math.PI );
+			this.uniforms[ 'seed_x' ].value = ThreeMath.randFloat( - 1, 1 );
+			this.uniforms[ 'seed_y' ].value = ThreeMath.randFloat( - 1, 1 );
+			this.uniforms[ 'distortion_x' ].value = ThreeMath.randFloat( 0, 1 );
+			this.uniforms[ 'distortion_y' ].value = ThreeMath.randFloat( 0, 1 );
 			this.curF = 0;
 			this.generateTrigger();
 
 		} else if ( this.curF % this.randX < this.randX / 5 ) {
 
 			this.uniforms[ 'amount' ].value = Math.random() / 90;
-			this.uniforms[ 'angle' ].value = Math.randFloat( - Math.PI, Math.PI );
-			this.uniforms[ 'distortion_x' ].value = Math.randFloat( 0, 1 );
-			this.uniforms[ 'distortion_y' ].value = Math.randFloat( 0, 1 );
-			this.uniforms[ 'seed_x' ].value = Math.randFloat( - 0.3, 0.3 );
-			this.uniforms[ 'seed_y' ].value = Math.randFloat( - 0.3, 0.3 );
+			this.uniforms[ 'angle' ].value = ThreeMath.randFloat( - Math.PI, Math.PI );
+			this.uniforms[ 'distortion_x' ].value = ThreeMath.randFloat( 0, 1 );
+			this.uniforms[ 'distortion_y' ].value = ThreeMath.randFloat( 0, 1 );
+			this.uniforms[ 'seed_x' ].value = ThreeMath.randFloat( - 0.3, 0.3 );
+			this.uniforms[ 'seed_y' ].value = ThreeMath.randFloat( - 0.3, 0.3 );
 
 		} else if ( this.goWild == false ) {
 
@@ -115,7 +115,7 @@ GlitchPass.prototype = Object.assign( Object.create( Pass.prototype ), {
 
 		for ( var i = 0; i < length; i ++ ) {
 
-			var val = Math.randFloat( 0, 1 );
+			var val = ThreeMath.randFloat( 0, 1 );
 			data_arr[ i * 3 + 0 ] = val;
 			data_arr[ i * 3 + 1 ] = val;
 			data_arr[ i * 3 + 2 ] = val;
